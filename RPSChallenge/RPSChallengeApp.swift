5dafadcd79f40eb91ccdaf3566cf759e6c402e76
Mac <macjacobson99@gmail.com>
2025-10-15 14:05:06 -0600

    Initial Commit

---
+++ RPSChallenge/RPSChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPSChallengeApp.swift
+//  RPSChallenge
+//
+//  Created by Jacobson, Machiah - Student on 10/15/25.
+//
+
+import SwiftUI
+
+@main
+struct RPSChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RPSChallengeTests/RPSChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  RPSChallengeTests.swift
+//  RPSChallengeTests
+//
+//  Created by Jacobson, Machiah - Student on 10/15/25.
+//
+
+import Testing
+@testable import RPSChallenge
+
+struct RPSChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
